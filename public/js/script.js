c49fb090ce66a84be5472fc72306f82876779f30
Takuya Nishino <>
2018-06-22 00:37:34 +0900

    2018/06/22 最低限の機能のみで公開しました。

--- public/js/script.js
+++ public/js/script.js
@@ -10,34 +10,115 @@
     data: {
       form: {
         name: '',
-        shortName: '',
-        backgroundColor: '',
-        themeColor: ''
+        short_name: '',
+        description: '',
+        start_url: '',
+        display: '',
+        orientation: '',
+        background_color: '',
+        theme_color: '',
+        icon_home: '',
+        icon_splash: ''
       },
-      maxLength: 12
+      maxLength: 12,
+      controls: [],
+      done: 0,
+      activated: {
+        name: false,
+        short_name: false,
+        description: false,
+        start_url: false,
+        display: false,
+        orientation: false,
+        background_color: false,
+        theme_color: false,
+        icon_home: false,
+        icon_splash: false
+      }
     },
-    beforeMount: function () {
-      var validation = this.validation
-      for (key in validation) {
-        validation[key] = true
+    beforeCreate: function () {
+      // $('#loader').delay(900).fadeOut(800);
+      var loader = document.getElementById('loader');
+      if (!loader.classList.contains('loader_close')) {
+        loader.classList.add('loader_close')
+      }
+    },
+    created: function () {
+      // controller push
+      var max = Object.keys(this.form).length;
+      for (let i = 0; i < max; i++) {
+        this.controls.push(false);
       }
     },
     computed: {
       validation: function () {
-        const form = this.form
+        var form = this.form
         return {
           name: (!!form.name),
-          shortName: (!!form.shortName && form.shortName.length <= this.maxLength),
-          backgroundColor: (!!form.backgroundColor),
-          themeColor: (!!form.themeColor)
+          short_name: (!!form.short_name && form.short_name.length <= this.maxLength),
+          description: (!!form.description),
+          start_url: (!!form.start_url),
+          display: (!!form.display),
+          orientation: (!!form.orientation),
+          background_color: (!!form.background_color),
+          theme_color: (!!form.theme_color),
+          icon_home: (!!form.icon_home),
+          icon_splash: (!!form.icon_splash)
         }
       },
-      isValid: function () {
+      isAllValid: function () {
         var validation = this.validation
         return Object.keys(validation).every(function (key) {
           return validation[key]
         })
       },
+      progressWidth: function () {
+        return {
+          width: (this.done / Object.keys(this.form).length * 100) + '%'
+        }
+      }
+    },
+    watch: {
+      'form.name': function () {
+        this.onChangeData(0, 'name')
+        this.activated.name = true
+      },
+      'form.short_name': function () {
+        this.onChangeData(1, 'short_name')
+        this.activated.short_name = true
+      },
+      'form.description': function () {
+        this.onChangeData(2, 'description')
+        this.activated.description = true
+      },
+      'form.start_url': function () {
+        this.onChangeData(3, 'start_url')
+        this.activated.start_url = true
+      },
+      'form.display': function () {
+        this.onChangeData(4, 'display')
+        this.activated.display = true
+      },
+      'form.orientation': function () {
+        this.onChangeData(5, 'orientation')
+        this.activated.orientation = true
+      },
+      'form.background_color': function () {
+        this.onChangeData(6, 'background_color')
+        this.activated.background_color = true
+      },
+      'form.theme_color': function () {
+        this.onChangeData(7, 'theme_color')
+        this.activated.theme_color = true
+      },
+      'form.icon_home': function () {
+        this.onChangeData(8, 'icon_home')
+        this.activated.icon_home = true
+      },
+      'form.icon_splash': function () {
+        this.onChangeData(9, 'icon_splash')
+        this.activated.icon_splash = true
+      }
     },
     filters: {
       emptyValidator: function (val) {
@@ -47,18 +128,55 @@
       }
     },
     methods: {
-      errorClassObject(key) {
+      errorClassObject (key) {
+        var activeCheck;
+        if (this.activated[key]) {
+          activeCheck = (this.validation[key] == false)
+        } else {
+          activeCheck = false
+        }
         return {
-          'is-invalid': (this.validation[key] == false)
+          'is-invalid': activeCheck
         }
       },
-      doSubmit() {
-        // POST処理
+      activeClassObject (key) {
+        var activeVal;
+        if (this.activated[key]) {
+          activeVal = this.validation[key]
+        } else {
+          activeVal = true
+        }
+        return !activeVal
+      },
+      onChangeData (index, key) {
+        this.controls[index] = this.validation[key];
+
+        let done = 0;
+        for (let i = 0; i < this.controls.length; i++) {
+          if (this.controls[i]) {
+            done++;
+          }
+        }
+        this.done = done;
+      },
+      doSubmit () {
+        var result = document.getElementById('result');
+        if (!result.classList.contains('open')) {
+          result.classList.add('open')
+        }
+
+        // smooth scroll
+        var target, targetTop;
+        target = result;
+        targetTop = (target) ? target.offsetTop : 0;
+        window.scrollTo({
+          top: targetTop,
+          left: 0,
+          behavior: 'smooth'
+        });
       }
     }
   });
-
-  console.log();
 
   function isEmpty(value){
     return !value;
